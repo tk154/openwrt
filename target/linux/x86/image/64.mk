8df0b8976975f032ffb3d42d234ef75c5a93bb33
Til Kaiser <mail@tk154.de>
2023-11-09 13:41:06 +0100

    x86: add support for Mellanox Spectrum Switches
    The Mellanox Spectrum Switches are Managed Ethernet Switches
    with speeds up to 800Gb/s. They utilize the switchdev API
    to offload the kernel's forwarding plane to a real ASIC.
    
    Tested on a Mellanox Spectrum SN2100 with the following specs:
     - CPU: Intel ATOM x86 dual-core 2.4GHz
     - RAM: 8GB
     - Disk: 16GB SSD
     - Ports: 16x QSFP28 100GbE, 1x 100M Mgmt Port, 1x RJ45 Serial Port
     - USB: 1x mini 2.0
     - Button: 1x (reset)
     - LEDs: 6x
    
    Installation:
     - Create a bootable USB device (either by flashing this image
       onto it or another Linux distribution)
     - Unzip the image
       (openwrt-x86-64-mlnx-spectrum-ext4-combined-efi.img.gz)
       and copy it onto the USB device
     - Plug the USB device into the Mellanox Switch and boot from it
     - Flash the image (e.g., with dd) onto the internal SSD
       of the switch (should be /dev/sda)
    
    To enter the BIOS, reboot the switch and press CTRL+B while you see
    the BIOS information text (American Megatrends …). The default password
    to enter the BIOS is 'admin'. To boot from the USB device, switch to the
    'Save & Exit' tab and select your USB device from the Boot Override options.
    
    Signed-off-by: Til Kaiser <mail@tk154.de>

--- target/linux/x86/image/64.mk
+++ target/linux/x86/image/64.mk
@@ -8,3 +8,46 @@ define Device/generic
   GRUB2_VARIANT := generic
 endef
 TARGET_DEVICES += generic
+
+define Device/mlnx-spectrum-common
+  DEVICE_VENDOR := Mellanox
+  DEVICE_MODEL := Spectrum
+  DEVICE_PACKAGES := \
+  kmod-mlxsw-spectrum kmod-mlxsw-minimal \
+  kmod-i2c-i801 kmod-leds-mlxcpld kmod-mlx_wdt kmod-mlxreg \
+  kmod-hwmon-coretemp kmod-hwmon-drivetemp kmod-hwmon-jc42
+  GRUB2_VARIANT := generic
+endef
+
+define Device/mlnx-spectrum
+  $(call Device/mlnx-spectrum-common)
+  DEVICE_MODEL := $$(DEVICE_MODEL) SN2000 Series
+  DEVICE_PACKAGES += \
+  kmod-e1000e kmod-igb \
+  mlxsw_spectrum-firmware kmod-mlxreg-sn2201
+endef
+TARGET_DEVICES += mlnx-spectrum
+
+define Device/mlnx-spectrum-2
+  $(call Device/mlnx-spectrum-common)
+  DEVICE_MODEL := $$(DEVICE_MODEL)-2 SN3000 Series
+  DEVICE_PACKAGES += \
+  kmod-igb mlxsw_spectrum2-firmware
+endef
+TARGET_DEVICES += mlnx-spectrum-2
+
+define Device/mlnx-spectrum-3
+  $(call Device/mlnx-spectrum-common)
+  DEVICE_MODEL := $$(DEVICE_MODEL)-3 SN4000 Series
+  DEVICE_PACKAGES += \
+  mlxsw_spectrum3-firmware kmod-mlxreg-lc
+endef
+TARGET_DEVICES += mlnx-spectrum-3
+
+define Device/mlnx-spectrum-4
+  $(call Device/mlnx-spectrum-common)
+  DEVICE_MODEL := $$(DEVICE_MODEL)-4 SN5000 Series
+  DEVICE_PACKAGES += \
+  mlxsw_spectrum4-firmware
+endef
+TARGET_DEVICES += mlnx-spectrum-4
